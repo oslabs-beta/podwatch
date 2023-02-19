4d3b7108fbca5d48cbc5b235e9aca240cd5505dd
Wvaviator <wvaviator@gmail.com>
2023-02-18 19:32:52 -0600

    set up mongoose and docker compose with mongodb

--- packages/webapp-server/src/index.ts
+++ packages/webapp-server/src/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3001, () => {
-  console.log('Server started on port 3001');
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || '');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log('Error connecting to MongoDB');
+    console.error(err);
+  }
+
+  app.listen(3001, () => {
+    console.log('Listening on port 3001');
+  });
+};
+
+start();
