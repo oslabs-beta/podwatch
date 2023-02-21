6b755505c21cd40c795c920efdf686a19689ab0d
Wvaviator <wvaviator@gmail.com>
2023-02-20 19:50:47 -0600

    add some scss example code

---
+++ packages/webapp-client/src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Logo.module.scss';
+
+const Logo = () => {
+  return <div className={styles.container}>Logo</div>;
+};
+
+export default Logo;
--- packages/webapp-client/src/pages/index.tsx
+++ packages/webapp-client/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.scss';
+import Logo from '../components/Logo/Logo';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Logo />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
