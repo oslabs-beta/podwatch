d6523dbf827c9824d00dcf126aae1099948c85b5
Wvaviator <wvaviator@gmail.com>
2023-02-21 21:57:43 -0600

    install and set up materialui and theming

--- packages/webapp-client/src/pages/_app.tsx
+++ packages/webapp-client/src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import '@/styles/globals.scss';
+import { createTheme, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
-import PageLayout from '../components/PageLayout/PageLayout';
+import PageLayout from '@/components/PageLayout/PageLayout';
+import variables from '@/styles/exports.module.scss';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: variables.colorPrimary,
+    },
+    secondary: {
+      main: variables.colorSecondary,
+    },
+  },
+  typography: {
+    fontFamily: variables.fontMain,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
+    <ThemeProvider theme={theme}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </ThemeProvider>
   );
 }
