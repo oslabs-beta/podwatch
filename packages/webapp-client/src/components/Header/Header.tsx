1d2d5b4f71b9c7ebccdf7c7d901d029818709b3c
Wvaviator <wvaviator@gmail.com>
2023-02-21 20:53:06 -0600

    set up header bar, layout, and css resets

---
+++ packages/webapp-client/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.contents}>Test</div>
+    </header>
+  );
+};
+
+export default Header;
--- packages/webapp-client/src/pages/_app.tsx
+++ packages/webapp-client/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import PageLayout from '../components/PageLayout/PageLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  );
 }
