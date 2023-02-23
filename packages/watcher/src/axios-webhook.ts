5fc3a1cc210ad81ddc2a19dc4963f7fecb593cb8
mashkn <mariya.novokhatska@gmail.com>
2023-02-22 19:26:36 -0700

    created general outline of the routing function

---
+++ packages/watcher/src/axios-webhook.ts
@@ -0,0 +1,29 @@
+const PODWATCH_CUSTOM_SERVER_URL = process.env.PODWATCH_CUSTOM_SERVER_URL;
+const PODWATCH_CLIENT_ID  = process.env.PODWATCH_CLIENT_ID ;
+const PODWATCH_CLIENT_SECRET = process.env.PODWATCH_CLIENT_SECRET;
+
+import axios from 'axios';
+
+const checkForCustomServer = async () => {
+    //check if the podwatch custom server url exists
+    try {
+        if(PODWATCH_CUSTOM_SERVER_URL){
+            //need to provide our PodWatch client ID and Secret
+            
+
+        } else {
+            //this means they are using their own custom server url
+            //do something
+
+            //send an axios webhook to their own server
+            axios.post('/', {
+
+            })
+        }
+    }
+    catch ( err: any ){
+        //do something with the error
+
+    }
+    
+}
