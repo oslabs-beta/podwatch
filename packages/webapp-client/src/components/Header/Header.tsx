34e98137712670a4f70a2ca9282325a79324e3d9
Wvaviator <wvaviator@gmail.com>
2023-02-23 19:49:10 -0600

    create navigation menu

--- packages/webapp-client/src/components/Header/Header.tsx
+++ packages/webapp-client/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
 import styles from './Header.module.scss';
 
 const Header = () => {
@@ -7,6 +8,13 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.contents}>
         <Logo />
+        <Navigation
+          items={[
+            { label: 'Docs', href: '/' },
+            { label: 'Examples', href: '/' },
+            { label: 'GitRepo', href: '/' },
+          ]}
+        />
       </div>
     </header>
   );
