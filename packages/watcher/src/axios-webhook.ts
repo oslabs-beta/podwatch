987aafb81a2b3b3bd2ce98ec8964b73aca5a28a6
mashkn <mariya.novokhatska@gmail.com>
2023-02-25 14:44:13 -0700

    Added value existence checks and a return type

--- packages/watcher/src/axios-webhook.ts
+++ packages/watcher/src/axios-webhook.ts
@@ -2,21 +2,26 @@ const PODWATCH_CUSTOM_SERVER_URL = process.env.PODWATCH_CUSTOM_SERVER_URL;
 const PODWATCH_CLIENT_ID  = process.env.PODWATCH_CLIENT_ID ;
 const PODWATCH_CLIENT_SECRET = process.env.PODWATCH_CLIENT_SECRET;
 
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const createAxiosInstance = () => {
+    //check if all 3 of the env file variables are present
+    if(PODWATCH_CUSTOM_SERVER_URL && PODWATCH_CLIENT_ID && PODWATCH_CLIENT_SECRET){
+        throw new Error('We have detected both a custom server URL and a PodWatch Client ID and Secret. You will need one or the other to continue.');
+        process.exit(1);
+    }
     //check if the podwatch custom server url exists
-        if(PODWATCH_CUSTOM_SERVER_URL){
+        else if(PODWATCH_CUSTOM_SERVER_URL){
             //need to provide our PodWatch client ID and Secret
-            const instance = axios.create({
+            const instanceCustom: AxiosInstance = axios.create({
                 baseURL: PODWATCH_CUSTOM_SERVER_URL,
                 timeout: 1000,
                 
             })
-            return instance;
+            return instanceCustom;
 
-        } else {
-            const instance = axios.create({
+        } else if(PODWATCH_CLIENT_ID && PODWATCH_CLIENT_SECRET) {
+            const instancePodWatch: AxiosInstance = axios.create({
                 baseURL: 'http://localhost:3001/watch',
                 timeout: 1000,
                 headers: {
@@ -24,8 +29,22 @@ const createAxiosInstance = () => {
                     clientSecret: PODWATCH_CLIENT_SECRET,
                 }
             })
-            return instance;
+            return instancePodWatch;
+        }
+        else if(!PODWATCH_CUSTOM_SERVER_URL || !PODWATCH_CLIENT_ID || !PODWATCH_CLIENT_SECRET) {
+            throw new Error('Your Custom Server URL, PodWatch Client ID and/or Secret are blank. Please review and try again.');
+            process.exit(1);
+        }
+        else {
+            throw new Error('Something went wrong while creating an Axios instance.');
+            process.exit(1);
         }
 }  
 
-export default createAxiosInstance();
+/*
+This function needs to be imported into the Event Dispatcher and then invoked. For example:
+    import createAxiosInstance from './axios-webhook.ts';
+    const instance = createAxiosInstance();
+    instance.get('/someURL')
+*/
+export default createAxiosInstance;
