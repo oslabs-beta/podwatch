b4e08cb34251534f305e51bad0e2e461c9351acd
Hidinkyu <Roysbae@gmail.com>
2023-03-02 19:49:47 -0800

    feature finished with landing layout for mobile view

--- packages/webapp-client/src/components/LandingContent/LandingContent.tsx
+++ packages/webapp-client/src/components/LandingContent/LandingContent.tsx
@@ -4,18 +4,20 @@ import { Chip } from '@mui/material';
 import Image from 'next/image';
 import codeSmith from '/public/images/logo/code-smith.png';
 import osLabs from '/public/images/logo/os-labs.png';
+import logo from '/public/images/logo/256.png';
 
 const LandingContent: React.FC = () => {
   return (
     <div className={styles.LandingContent}>
       <div className={styles.text}>
-        <h2>PodWatch</h2>
+        <h1>PodWatch</h1>
         <h1>Kubernetes Error Hub</h1>
+        <Chip label="V0.0.1" color="primary" />
       </div>
-      <Chip label="V0.0.1" color="primary" />
-      <span className="imgs">
-        <Image className={styles.logo} src={codeSmith} alt="CodeSmith" />
-        <Image className={styles.logo} src={osLabs} alt="OSLabs" />
+      <Image className={styles.logo} src={logo} alt="podWatch" />
+      <span className={styles.logoBox}>
+        <Image className={styles.schoolLogo} src={codeSmith} alt="CodeSmith" />
+        <Image className={styles.schoolLogo} src={osLabs} alt="OSLabs" />
       </span>
     </div>
   );
