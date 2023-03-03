351e84db6ca17f38ed01871886364ae4781fbbb3
Wvaviator <wvaviator@gmail.com>
2023-03-03 13:18:09 -0600

    add logs and config for hosting outside a cluster

--- packages/watcher/src/index.ts
+++ packages/watcher/src/index.ts
@@ -4,14 +4,18 @@ import webhookInstance from './axios-webhook';
 import eventDispatcher from './event-dispatcher';
 import { KEventReceiver } from './EventReceiver';
 
+console.log('Instantiating JSON stream parser');
 const jsonStreamParser = new JsonStreamParser();
 
+console.log('Instantiating event dispatcher');
 const dispatch = eventDispatcher(webhookInstance);
 
+console.log('Instantiating event receiver');
 const receiver = new KEventReceiver(
   kubernetesInstance,
   jsonStreamParser,
   dispatch
 );
 
+console.log('Starting event receiver');
 receiver.start();
