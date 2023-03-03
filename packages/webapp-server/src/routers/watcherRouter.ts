fc90b8099ebc12d951a899e10acb66a17ac6e45d
Wvaviator <wvaviator@gmail.com>
2023-03-02 23:00:36 -0600

    edits to get podwatch running within cluster

--- packages/webapp-server/src/routers/watcherRouter.ts
+++ packages/webapp-server/src/routers/watcherRouter.ts
@@ -1,15 +1,10 @@
 import { Request, Response, Router } from 'express';
 
-const watcherRouter = Router();
+export const watcherRouter = Router();
 
-watcherRouter.post(
-  '/',
-  kErrorController.getCluster,
-  kErrorController.saveAll,
-  (req: Request, res: Response) => {
-    console.log(req.body);
-    res.status(200).send('OK');
-  }
-);
+watcherRouter.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send('OK');
+});
 
 export default watcherRouter;
