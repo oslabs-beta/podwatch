17c52b0f230963b2fa7f323f37e7005460ca314c
Wvaviator <wvaviator@gmail.com>
2023-03-04 12:58:06 -0600

    add route/controller to fetch errors from database

--- packages/webapp-server/src/routers/watcherRouter.ts
+++ packages/webapp-server/src/routers/watcherRouter.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from 'express';
 
-export const watcherRouter = Router();
+const watcherRouter = Router();
 
-watcherRouter.post('/', (req: Request, res: Response) => {
-  console.log(req.body);
-  res.send('OK');
-});
+watcherRouter.post(
+  '/',
+  kErrorController.getCluster,
+  kErrorController.saveAll,
+  (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(200).send('OK');
+  }
+);
 
 export default watcherRouter;
