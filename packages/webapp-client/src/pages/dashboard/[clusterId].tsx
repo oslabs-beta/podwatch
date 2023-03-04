059f622220e763ad90cff453446e6cc69a412cd2
Wvaviator <wvaviator@gmail.com>
2023-03-04 13:24:45 -0600

    set up dashboard page with some dummy data

---
+++ packages/webapp-client/src/pages/dashboard/[clusterId].tsx
@@ -0,0 +1,49 @@
+import { GetServerSideProps, NextPage } from 'next';
+import React from 'react';
+import { Cluster } from '../../types/Cluster';
+import { KError } from '../../types/KError';
+
+interface DashboardPageProps {
+  cluster: Cluster;
+  kErrors: KError[];
+}
+
+const Dashboard: NextPage<DashboardPageProps> = ({ cluster, kErrors }) => {
+  return <div>Dashboard</div>;
+};
+
+export const getServerSideProps: GetServerSideProps<
+  DashboardPageProps
+> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const clusterId = params.clusterId as string;
+
+  //TODO: Prefetch cluster data to serve as props
+  const cluster: Cluster = {
+    id: clusterId,
+    name: 'Cluster 1',
+    description: 'This is a cluster',
+    owner: {
+      id: '1',
+      email: 'a@a.a',
+      provider: 'local',
+    },
+    members: [],
+  };
+
+  const kErrors: KError[] = [];
+
+  return {
+    props: {
+      cluster,
+      kErrors,
+    },
+  };
+};
+
+export default Dashboard;
