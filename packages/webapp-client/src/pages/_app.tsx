099da813ece62df61abab749f1d48fec5d2ed1b7
Katherine Marrow <kmcromer1@gmail.com>
2023-03-06 22:33:09 -0500

    working on route

--- packages/webapp-client/src/pages/_app.tsx
+++ packages/webapp-client/src/pages/_app.tsx
@@ -3,13 +3,35 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import ThemeProvider from '../context/ThemeProvider/ThemeProvider';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ClusterMain from '@/routes/ClusterMain';
+import React from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: '/clustermain',
+    element: <ClusterMain />,
+  },
+]);
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <ThemeProvider>
+//       <PageLayout>
+//         <Component {...pageProps} />
+//       </PageLayout>
+//     </ThemeProvider>
+//   );
+// }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <PageLayout>
-        <Component {...pageProps} />
-      </PageLayout>
-    </ThemeProvider>
+    <React.StrictMode>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+        <PageLayout>
+          <Component {...pageProps} />
+        </PageLayout>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
--- packages/webapp-client/src/pages/index.tsx
+++ packages/webapp-client/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './_app';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>Main body</main>
     </>
   );
