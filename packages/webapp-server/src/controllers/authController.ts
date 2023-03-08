aa82826ee0f80a3dc2a610f5e8d5072e65c71070
mashkn <mariya.novokhatska@gmail.com>
2023-03-07 19:49:15 -0700

    progress from tuesday

--- packages/webapp-server/src/controllers/authController.ts
+++ packages/webapp-server/src/controllers/authController.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserModel } from '../models/UserModel';
+import jwt from "jsonwebtoken";
+import { UserDocument } from '../models/UserModel';
+import passport from 'passport';
 
 /**
  * This middleware function will log a user in with the provided email and password. It will then set the user's id on the session and set the user on res.locals.user. It will then call next() to allow the request to continue. If the email or password are not provided, this function will throw an error. If the email is not found in the database, or if the password does not match the password in the database, this function will throw an error.
@@ -28,8 +31,9 @@ export const loginWithEmailAndPw = async (
       throw new Error('Invalid email or password');
     }
 
-    req.session.userId = user.id;
-    res.locals.user = user;
+    generateJwt(user, res);
+
+    req.user = user;
 
     return next();
     }
@@ -68,41 +72,39 @@ export const registerWithEmailAndPw = async (
 
     await user.save();
 
-    res.locals.user = user;
+    generateJwt(user, res);
 
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-};
-
-/**
- * This middleware function closes the gap between OAuth and traditional login. If the user is not using OAuth, the the req.user will be undefined. The req.user will then be populated with the user's information by using the userID property on req.session. If there is no req.session.userID or there is not matching user in the database, this function will not error. It will simply call next() and allow the request to continue. Use this middleware with each request before using any other middleware that accesses user data on the req.user property.
- * @param req
- * @param res
- * @param next
- * @returns
- */
-export const getUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { userId } = req.session;
-    if (!userId) {
-      return next();
-    }
-    const user = await UserModel.findById(userId);
-    if (!user) {
-      return next();
-    }
     req.user = user;
+
     return next();
   } catch (error) {
     return next(error);
   }
 };
+
+export const generateJwt = (req: Request, res: Response, next: NextFunction) => {
+
+  const user = req.user;
+
+  if (!user) {
+    return next({
+      
+    })
+  }
+
+  const token = jwt.sign({
+    sub: user.id,
+    email: user.email
+  }, process.env.JWT_TOKEN || 'abc');
+
+  res.cookie('podwatch_jwt', token, {
+    httpOnly: true,
+    secure: true,
+    signed: true,
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  });
+
+}
 
 /**
  * This middleware function will redirect the user to the login page if they are not authenticated. If they are authenticated, it will call next() and allow the request to continue. Note that if the route using this middleware is called by an AJAX request, the user will not be redirected. Instead, the request will fail with a 401 status code.
@@ -116,9 +118,12 @@ export const authenticateUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  if (req.user) {
-    console.log('Authenticated user: ', req.user);
-    return next();
-  }
-  res.status(401).redirect('/signin');
+  // if (req.user) {
+  //   console.log('Authenticated user: ', req.user);
+  //   return next();
+  // }
+  // res.status(401).redirect('/signin');
+
+  const passportMiddleware = passport.authenticate('jwt', { session: false });
+  return passportMiddleware(req, res, next);
 };
---
+++ packages/webapp-server/src/routes/authRouter.ts
@@ -0,0 +1,8 @@
+import { Request, Response, Router } from 'express';
+import passport from 'passport';
+
+const router = Router();
+
+
+
+export default router;
