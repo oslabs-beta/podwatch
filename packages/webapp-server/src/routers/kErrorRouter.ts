0b93d5fb632eb086aacadbc9ee9fb938fb4a0cce
mashkn <mariya.novokhatska@gmail.com>
2023-03-07 22:06:36 -0700

    working auth strategy, router, private hashed pw info

--- packages/webapp-server/src/routers/kErrorRouter.ts
+++ packages/webapp-server/src/routers/kErrorRouter.ts
@@ -1,19 +1,22 @@
 import { Router } from 'express';
 import { kErrorController } from '../controllers/kErrorController';
+import { authenticateUser } from '../controllers/authController';
 
 const kErrorRouter = Router();
 
 kErrorRouter.get(
   '/',
+  authenticateUser,
   kErrorController.getCluster,
   kErrorController.getMany,
   (req, res) => {
-    res.status(200).json(res.locals.kErrors);
+    return res.status(200).json(res.locals.kErrors);
   }
 );
 
 kErrorRouter.get(
   '/:id',
+  authenticateUser,
   kErrorController.getCluster,
   kErrorController.getOne,
   (req, res) => {
