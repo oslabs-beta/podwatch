b8c20085b560bc9bc930fdd3d59aa89307153564
Katherine Marrow <kmcromer1@gmail.com>
2023-03-07 22:59:55 -0500

    Co-authored-by: drewluu <drewluu@users.noreply.github.com> Co-authored-by: Hidinkyu <Hidinkyu@users.noreply.github.com>

---
+++ packages/webapp-client/src/components/ClusterManagementContent/ClusterManagement.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styles from './CluterManagement.module.scss';
+import Cluster from '../../../../webapp-server/src/models/ClusterModel';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2';
+
+interface ClusterProps {
+  owner: string;
+}
+
+const ClusterManagement: React.FC<ClusterProps> = () => {
+  //const [clusters, setClusters] = React.useState<Cluster[]>;
+  //   React.useEffect(() => {
+  //     const fetchClusters = async () => {
+  //       const getClusters = await fetch('/clusters', {
+  //         method: 'GET',
+  //         headers: {
+  //           owner,
+  //           'Content-Type': 'application/json',
+  //         },
+  //       });
+  //       const userClusters = await getClusters.json();
+  //       setClusters(userClusters);
+  //     };
+  //   }, []);
+  const clusters = [
+    { name: 'Codesmith Project', owner: 'Codesmith', id: '1234' },
+    { name: 'Codesmith Project2', owner: 'Codesmith', id: '1235' },
+  ];
+  return (
+    <>
+      <div className="spacer" />
+      <div className="ClusterManagement">
+        <div>
+          {clusters.map((cluster: any) => {
+            return (
+              <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+              >
+                <div key={cluster.id}>
+                  <Grid>
+                    <Card sx={{ m: 1 }} className={styles.card}>
+                      <Typography
+                        variant="body1"
+                        fontSize="large"
+                        className={styles.name}
+                      >
+                        {cluster.name}
+                      </Typography>
+                      <Typography className={styles.date}>
+                        Last Issued March 7 2023
+                      </Typography>
+                    </Card>
+                  </Grid>
+                </div>
+              </Grid>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ClusterManagement;
---
+++ packages/webapp-client/src/pages/clusterMan.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ClusterManagement from '../components/ClusterManagementContent/ClusterManagement';
+
+export default function clusterman() {
+  return (
+    <>
+      <ClusterManagement />
+    </>
+  );
+}
