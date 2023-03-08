20842eda7dea36e5d031efdbf8608fed5ae65ead
mashkn <mariya.novokhatska@gmail.com>
2023-03-07 22:06:36 -0700

    working auth strategy, router, private hashed pw info

--- packages/webapp-server/src/controllers/authController.ts
+++ packages/webapp-server/src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserModel } from '../models/UserModel';
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import { UserDocument } from '../models/UserModel';
 import passport from 'passport';
 
@@ -16,29 +16,51 @@ export const loginWithEmailAndPw = async (
   res: Response,
   next: NextFunction
 ) => {
+  const { email, password } = req.body; // Abc123
+  if (!email || !password) {
+    return next({
+      log: 'Email or password missing from request body.',
+      message: 'Invalid credentials',
+      status: 403,
+    });
+  }
+
   try {
-    const { email, password } = req.body; // Abc123
-    if (!email || !password) {
-      throw new Error('Email and password are required');
-    }
-
+    console.log(`Searching for ${email} in the db`);
     const user = await UserModel.findOne({ email });
+
+    console.log(`User ${user?.id} found.`);
+
     if (!user) {
-      throw new Error('Invalid email or password');
-    } else {
-        const userAuthenticated = await user.comparePassword(password);
-    if (!userAuthenticated) {
-      throw new Error('Invalid email or password');
+      return next({
+        log: 'Attempted sign in failed, no matching user in db',
+        message: 'Invalid credentials',
+        status: 403,
+      });
     }
 
-    generateJwt(user, res);
+    console.log('Comparing password');
+    const userAuthenticated = await user.comparePassword(password);
+    if (!userAuthenticated) {
+      return next({
+        log: 'Attempted sign in failed, wrong password',
+        message: 'Invalid credentials',
+        status: 403,
+      });
+    }
+
+    console.log('Assigning req.user');
 
     req.user = user;
 
     return next();
-    }
   } catch (error) {
-    return next(error);
+    return next({
+      log: 'Error occurred while attempting to sign in',
+      message: 'Unexpected error occurred',
+      status: 500,
+      error,
+    });
   }
 };
 
@@ -54,17 +76,25 @@ export const registerWithEmailAndPw = async (
   res: Response,
   next: NextFunction
 ) => {
+  console.log('Registering new user...');
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next({
+      log: 'Email or password missing from request body.',
+      message: 'Invalid credentials',
+      status: 403,
+    });
+  }
+
+  // TODO: Validate email is in correct format and password is long enough with required characters
+
+  // TODO: Check if email is already in use
+
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      throw new Error('Email and password are required');
-    }
+    console.log(`Creating user ${email}`);
 
-    // TODO: Validate email is in correct format and password is long enough with required characters
-
-    // TODO: Check if email is already in use
-
-    const user = new UserModel({
+    const user = UserModel.build({
       email,
       password,
       provider: 'local',
@@ -72,39 +102,59 @@ export const registerWithEmailAndPw = async (
 
     await user.save();
 
-    generateJwt(user, res);
+    console.log('User created and saved');
 
     req.user = user;
 
     return next();
   } catch (error) {
-    return next(error);
+    return next({
+      error,
+      log: 'An error occurred trying to create new user',
+      message: 'User account creation failed',
+      status: 500,
+    });
   }
 };
 
-export const generateJwt = (req: Request, res: Response, next: NextFunction) => {
-
-  const user = req.user;
+export const generateJwt = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user as UserDocument;
 
   if (!user) {
     return next({
-      
-    })
+      log: 'No user on request object, cannot create JWT',
+      message: 'You must log in to use this',
+      status: 403,
+    });
   }
 
-  const token = jwt.sign({
-    sub: user.id,
-    email: user.email
-  }, process.env.JWT_TOKEN || 'abc');
+  console.log(`Generating JWT for user ${user.email}`);
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      email: user.email,
+    },
+    process.env.JWT_TOKEN || 'abc'
+  );
+
+  console.log(`Token generated: ${token}`);
 
   res.cookie('podwatch_jwt', token, {
-    httpOnly: true,
-    secure: true,
+    // httpOnly: true,
+    // secure: true,
     signed: true,
     maxAge: 1000 * 60 * 60 * 24, // 1 day
   });
 
-}
+  console.log('Cookie assigned');
+
+  return next();
+};
 
 /**
  * This middleware function will redirect the user to the login page if they are not authenticated. If they are authenticated, it will call next() and allow the request to continue. Note that if the route using this middleware is called by an AJAX request, the user will not be redirected. Instead, the request will fail with a 401 status code.
@@ -118,12 +168,6 @@ export const authenticateUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  // if (req.user) {
-  //   console.log('Authenticated user: ', req.user);
-  //   return next();
-  // }
-  // res.status(401).redirect('/signin');
-
   const passportMiddleware = passport.authenticate('jwt', { session: false });
   return passportMiddleware(req, res, next);
 };
--- packages/webapp-server/src/routers/kErrorRouter.ts
+++ packages/webapp-server/src/routers/kErrorRouter.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express';
 import { kErrorController } from '../controllers/kErrorController';
+import { authenticateUser } from '../controllers/authController';
 
 const kErrorRouter = Router();
 
 kErrorRouter.get(
   '/',
+  authenticateUser,
   kErrorController.getCluster,
   kErrorController.getMany,
   (req, res) => {
-    res.status(200).json(res.locals.kErrors);
+    return res.status(200).json(res.locals.kErrors);
+  }
+);
+
+kErrorRouter.get(
+  '/:id',
+  authenticateUser,
+  kErrorController.getCluster,
+  kErrorController.getOne,
+  (req, res) => {
+    res.status(200).json(res.locals.kError);
   }
 );
 
