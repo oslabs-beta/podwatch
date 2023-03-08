5c582a6ab91e7578245c9d582f9e174546873eb1
Katherine Marrow <kmcromer1@gmail.com>
2023-03-07 20:03:17 -0500

    working on router

--- packages/webapp-client/src/routes/ClusterMain.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ClusterSummary from '../pages/clusterManagement/clusterManagement';
-const ClusterMain = () => {
-  return <ClusterSummary />;
-};
-
-export default ClusterMain;
