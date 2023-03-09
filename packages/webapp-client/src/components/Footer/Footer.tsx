0b42521a42e7c4bf6d6a2e034f04ef290ae524e2
Hidinkyu <Roysbae@gmail.com>
2023-03-08 17:19:50 -0800

    desktop footer setup

--- packages/webapp-client/src/components/Footer/Footer.tsx
+++ packages/webapp-client/src/components/Footer/Footer.tsx
@@ -7,28 +7,31 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-
-      <div className={styles.link}>
-        <Link href="/">
-          <GitHubIcon className={styles.icon} />
-        </Link>
+    <>
+      <div className={styles.footer}>
+        <div className={styles.desktop}>
+          
+        </div>
+        <div className={styles.link}>
+          <Link href="/">
+            <GitHubIcon className={styles.icon} />
+          </Link>
           <p className={styles.iconText}>Github</p>
-      </div>
-      <div className={styles.link}>
-        <Link href="/">
-          <AutoStoriesIcon className={styles.icon} />
-        </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="/">
+            <AutoStoriesIcon className={styles.icon} />
+          </Link>
           <p className={styles.iconText}>Docs</p>
-      </div>
-      <div className={styles.link}>
-        <Link href="/">
-          <PeopleAltIcon className={styles.icon} />
-        </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="/">
+            <PeopleAltIcon className={styles.icon} />
+          </Link>
           <p className={styles.iconText}>The Team</p>
+        </div>
       </div>
-      
-    </div>
+    </>
   );
 };
 
