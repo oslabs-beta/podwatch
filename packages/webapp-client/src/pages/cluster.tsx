450cb5910eaf9a6366ae57899dd404b7523bf136
Hidinkyu <Roysbae@gmail.com>
2023-03-10 12:29:24 -0800

    Refactoring mobile view

--- packages/webapp-client/src/pages/cluster.tsx
+++ packages/webapp-client/src/pages/cluster.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ClusterManagement from '../../components/ClusterManagementContent/ClusterManagement';
-import { User } from '../../types/User';
+import ClusterManagement from '../components/ClusterManagementContent/ClusterManagement';
+import { User } from '../types/User';
 
 interface clusterManProps {
   owner: User;
