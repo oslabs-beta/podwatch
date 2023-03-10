28bbee89cd92cdeb65170f401abe758a7685a57f
Hidinkyu <Roysbae@gmail.com>
2023-03-10 10:33:45 -0800

    modularizing footer

--- packages/webapp-client/src/components/Footer/Footer.tsx
+++ packages/webapp-client/src/components/Footer/Footer.tsx
@@ -1,105 +1,142 @@
 import React from 'react';
 
-import GitHubIcon from '@mui/icons-material/GitHub';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PublicIcon from '@mui/icons-material/Public';
-import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 import styles from './Footer.module.scss';
 
 import Link from 'next/link';
 
-import logo from '/public/images/logo/512.png';
 import Alex from '/public/images/team/Alex.jpg';
 import Andrew from '/public/images/Team/Andrew.jpg';
 import Katherine from '/public/images/Team/Katherine.png';
 import Maria from '/public/images/Team/Maria.png';
 import Roy from '/public/images/Team/Roy.jpg';
 
+import TeamLink from '../TeamLink/TeamLink';
+
 const Footer = () => {
+  const teamLinks = [
+    {
+      image: Alex.src,
+      github: 'https://github.com/WVAviator',
+      linkedin: 'https://www.linkedin.com/in/wvaviator/',
+      personal: 'https://www.wvaviator.com',
+    },
+    {
+      image: Andrew.src,
+      github: 'https://github.com/drewluu',
+      linkedin: 'https://www.linkedin.com/in/andrew-luu-143113b5/',
+      personal: 'drewluu24@gmail.com',
+    },
+    {
+      image: Katherine.src,
+      github: 'https://github.com/',
+      linkedin: 'https://www.linkedin.com/in/',
+      personal: 'kmcromer1@gmail.com',
+    },
+    {
+      image: Maria.src,
+      github: 'https://github.com/mashkn',
+      linkedin: 'https://www.linkedin.com/in/mariyanovok/',
+      personal: 'mariya.novokhatska@gmail.com',
+    },
+    {
+      image: Roy.src,
+      github: 'https://github.com/Hidinkyu',
+      linkedin: 'https://www.linkedin.com/in/roysbae/',
+      personal: 'roysbae@gmail.com',
+    },
+  ];
   return (
     <>
       <div className={styles.footer}>
         <div className={styles.desktop}>
           <div className={styles.logo}></div>
           <div className={styles.teamLinks}>
-            <div className={styles.teamLink}>
+            {teamLinks.map((link) => (
+              <TeamLink
+                image={link.image}
+                github={link.github}
+                linkedin={link.linkedin}
+                personal={link.personal}
+              />
+            ))}
+
+            {/* <div className={styles.teamLink}>
               <img draggable={false} src={Alex.src} alt="Alex" />
               <Link href="https://github.com/WVAviator" target="_blank">
-                <GitHubIcon /> /WVAviator
+                <GitHubIcon /> 
               </Link>
 
               <Link
                 href="https://www.linkedin.com/in/wvaviator/"
                 target="_blank"
               >
-                <LinkedInIcon /> /wvaviator/
+                <LinkedInIcon />
               </Link>
               <Link href="https://www.wvaviator.com" target="_blank">
-                <PublicIcon /> wvaviator
+                <PublicIcon /> 
               </Link>
             </div>
             <div className={styles.teamLink}>
               <img draggable={false} src={Andrew.src} alt="Andrew" />
               <Link href="https://github.com/drewluu" target="_blank">
-                <GitHubIcon /> /drewluu
+                <GitHubIcon /> 
               </Link>
               <Link
                 href="https://www.linkedin.com/in/andrew-luu-143113b5/"
                 target="_blank"
               >
-                <LinkedInIcon /> /andrew-luu-143113b5/
+                <LinkedInIcon /> 
               </Link>
-              <p>
-                <EmailIcon /> drewluu24@gmail.com
-              </p>
+              <Link href="mailto:drewluu24@gmail.com">
+                <EmailIcon /> 
+              </Link>
             </div>
             <div className={styles.teamLink}>
               <img draggable={false} src={Katherine.src} alt="Katherine" />
               <Link href="https://github.com/kmarrow1" target="_blank">
-                <GitHubIcon /> /kmarrow1
+                <GitHubIcon />
               </Link>
               <Link
                 href="http://www.linkedin.com/in/katherine-marrow-668a8a1a6"
                 target="_blank"
               >
-                <LinkedInIcon /> /katherine-marrow
+                <LinkedInIcon />
               </Link>
-              <p>
-                <EmailIcon /> kmcromer1@gmail.com
-              </p>
+              <Link href="mailto:kmcromer1@gmail.com">
+                <EmailIcon />
+              </Link>
             </div>
             <div className={styles.teamLink}>
               <img draggable={false} src={Maria.src} alt="Maria" />
               <Link href="https://github.com/mashkn" target="_blank">
                 <GitHubIcon />
-                /mashkn
               </Link>
               <Link
                 href="https://www.linkedin.com/in/mariyanovok/"
                 target="_blank"
               >
-                <LinkedInIcon /> /mariyanovok/
+                <LinkedInIcon />
               </Link>
-              <p>
-                <EmailIcon /> mariya.novokhatska@gmail.com
-              </p>
+              <Link href="mailto:mariya.novokhatska@gmail.com">
+                <EmailIcon />
+              </Link>
             </div>
             <div className={styles.teamLink}>
               <img draggable={false} src={Roy.src} alt="Roy" />
               <Link href="https://github.com/Hidinkyu" target="_blank">
                 <GitHubIcon />
-                /Hidinkyu
               </Link>
               <Link href="https://www.linkedin.com/in/roysbae/" target="_blank">
-                <LinkedInIcon /> /roysbae/
+                <LinkedInIcon />
               </Link>
-              <p>
-                <EmailIcon /> roybae@gmail.com
-              </p>
-            </div>
+              <Link href="roybae@gmail.com">
+                <EmailIcon />
+              </Link>
+            </div> */}
           </div>
         </div>
         <div className={styles.link}>
